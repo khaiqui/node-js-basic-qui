d96b82add766f0955233806f256f01d315560760
khaiqui <nguyenkhaiqui2003@gmail.com>
2024-05-23 23:42:58 +0700

    l5

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
-})
+// const server = http.createServer((req, res) => {
+//     console.log('run request ...')
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h3>Hello world! </h3>');
+//     res.write('<h2>from ERIC & HOI DAN IT </h2>');
+//     res.end();
+// })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
-})
+// server.listen(3000, 'localhost', () => {
+//     console.log('Node.JS server is running on port: 3000');
+// })
 
 // const http = require('http');
 
@@ -25,3 +25,20 @@ server.listen(3000, 'localhost', () => {
 // server.listen(3000, 'localhost', () => {
 //     console.log('Node.JS server is running on port: 3000');
 // })
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('My name is Qui!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
